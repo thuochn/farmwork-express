89f17ea4a36c195bed68f6c837c6f6e4671e498d
truong <hoangvantruonghn2@gmail.com>
2020-05-28 12:12:59 +0700

    Create Book

--- index.js
+++ index.js
@@ -1,6 +1,10 @@
 const express = require("express");
 const app = express();
+const bodyParser = require('body-parser');
+
 const pug = require("pug");
+const shortid = require("shortid");
+
 const low = require("lowdb");
 const FileSync = require('lowdb/adapters/FileSync');
  
@@ -12,10 +16,23 @@ const port = 3000;
 app.set('view engine', 'pug');
 app.set('views', './views');
 
+app.use(bodyParser.urlencoded({ extended: false }))
+app.use(bodyParser.json())
+
 app.get("/", function(req, res) {
 	res.render("index", {
 		products: db.get('products').value()
 	});
+});
+
+app.get("/create", function(req, res) {
+	res.render("create")
+});
+
+app.post("/create", function(req, res) {
+	req.body.id = shortid.generate();
+	db.get('products').push(req.body).write()
+	res.redirect('/')
 });
 
 app.listen(port, function(req, res) {
