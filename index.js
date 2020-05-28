d84cd57efe9a29ccbc2ba6428aa93580f1aa179f
truong <hoangvantruonghn2@gmail.com>
2020-05-29 01:21:12 +0700

    router

--- index.js
+++ index.js
@@ -1,15 +1,12 @@
 const express = require("express");
-const app = express();
 const bodyParser = require('body-parser');
+const pug = require('pug');
 
-const pug = require("pug");
-const shortid = require("shortid");
+const userRouter = require("./router/router.users");
+const bookRouter = require("./router/router.books")
+const db = require("./db");
 
-const low = require("lowdb");
-const FileSync = require('lowdb/adapters/FileSync');
- 
-const adapter = new FileSync('db.json');
-const db = low(adapter);
+const app = express();
 
 const port = 3000;
 
@@ -25,109 +22,8 @@ app.get("/", function(req, res) {
 	});
 });
 
-app.get("/books/create", function(req, res) {
-	res.render("create")
-});
-
-app.post("/books/create", function(req, res) {
-	req.body.id = shortid.generate();
-	db.get('products')
-	.push(req.body)
-	.write()
-	res.redirect('/')
-});
-
-app.get("/books/search", function(req, res) {
-	let q = req.query.q;
- 	let matchedBook = db.get("products").value().filter(function(product) {
- 		return product.title.toLowerCase().indexOf(q.toLowerCase()) !== -1;
- 	});
-	res.render('index',{
- 		products: matchedBook
-	});
-});
-
-app.get("/books/view/:id", function(req, res) {
-	let id = req.params.id;
-	console.log(id);
- 	let product = db.get('products').find({ id: id }).value();
- 	res.render('view', {
- 		product: product
- 	});
-});
-
-app.get("/books/view/:id/delete",function(req, res) {
-	let id = req.params.id;
-	db.get('products')
-	.remove({ id: id })
-	.write()
-	res.redirect("/");
-});
-
-app.post("/books/view/:id/update", function(req, res) {
-	let id = req.params.id;
-	let title = req.body.title;
-	db.get('products')
-	.find({ id: id })
-	.assign({ title: title })
-	.write()
-	res.redirect("/")
- });
-
-app.get("/users", function(req, res) {
-	res.render('users/index',{
-		users: db.get('users').value()
-	});
-});
-
-app.get("/users/create", function(req, res) {
-	res.render('users/create')
-});
-
-app.post("/users/create", function(req, res) {
-	req.body.id = shortid.generate();
-	db.get('users')
-	.push(req.body)
-	.write()
-	res.redirect('/users')
-});
-
-app.get("/users/search", function(req, res) {
-	let q = req.query.q;
- 	let matchedUser = db.get("users").value().filter(function(user) {
- 		return user.name.toLowerCase().indexOf(q.toLowerCase()) !== -1;
- 	});
-	res.render('users/index',{
- 		users: matchedUser
-	});
-});
-
-app.get("/users/view/:id", function(req, res) {
-	let id = req.params.id;
- 	let user = db.get("users").find({ id: id }).value();
- 	res.render('users/view', {
- 		user: user
- 	});
-});
-
-app.post("/users/view/:id/update", function(req, res) {
-	let id = req.params.id;
-	db.get('users')
-	.find({ id: id })
-	.assign({ name: req.body.name })
-	.assign({ phonel: req.body.phonel })
-	.assign({ date: req.body.date })
-	.write()
-	res.redirect("/users")
- });
-
-app.get("/users/view/:id/delete", function(req, res) {
-	let id = req.params.id;
-	db.get('users')
-	  .remove({ id: id })
-	  .write()
-	 res.redirect("/users")
-})
+app.use("/books", bookRouter);
+app.use("/users", userRouter);
 
 app.listen(port, function(req, res) {
 	console.log("localhost:" + port)
---
+++ router/router.books.js
@@ -0,0 +1,57 @@
+const express = require('express');
+const shortid = require("shortid");
+const bodyParser = require('body-parser');
+const db = require("../db");
+
+const router = express.Router();
+
+router.get("/create", function(req, res) {
+	res.render("create")
+});
+
+router.post("/create", function(req, res) {
+	req.body.id = shortid.generate();
+	db.get('products')
+	.push(req.body)
+	.write()
+	res.redirect('/')
+});
+
+router.get("/search", function(req, res) {
+	let q = req.query.q;
+ 	let matchedBook = db.get("products").value().filter(function(product) {
+ 		return product.title.toLowerCase().indexOf(q.toLowerCase()) !== -1;
+ 	});
+	res.render('index',{
+ 		products: matchedBook
+	});
+});
+
+router.get("/view/:id", function(req, res) {
+	let id = req.params.id;
+	console.log(id);
+ 	let product = db.get('products').find({ id: id }).value();
+ 	res.render('view', {
+ 		product: product
+ 	});
+});
+
+router.get("/view/:id/delete",function(req, res) {
+	let id = req.params.id;
+	db.get('products')
+	.remove({ id: id })
+	.write()
+	res.redirect("/");
+});
+
+router.post("/view/:id/update", function(req, res) {
+	let id = req.params.id;
+	let title = req.body.title;
+	db.get('products')
+	.find({ id: id })
+	.assign({ title: title })
+	.write()
+	res.redirect("/")
+ });
+
+ module.exports = router;
